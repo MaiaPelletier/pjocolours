7e30faa11387a5724eb3ad5546ea8dad30f76802
Maia Pelletier <44327546+MaiaPelletier@users.noreply.github.com>
2020-03-30 16:03:06 -0400

    Initial commit

---
+++ R/pjo_palettes.R
@@ -0,0 +1,13 @@
+#' Complete list of palettes
+#'
+#' For the time being, all palettes only contain 5 colours.
+#' Palettes are derived using coolor.co and making personal adjustments.
+#'
+#' @export
+pjo_palettes <- list(
+  LightningThief = c('#3e685e', '#91c4af', '#d1e597', '#edd57d', '#d6b548'),
+  SeaOfMonsters = c("#E36726", "#7D2416", "#7A412F", "#B64824", "#FAA149"),
+  TitansCurse = c("#0F396B", "#628FC4", "#B7ADC5", "#E3E0F1", "#30596D"),
+  BatOfTheLabyrinth = c("#89211E", "#E68369", "#CA6328", "#FECA95", "#592124"),
+  LastOlympian = c("#4C6155", "#737A76", "#B59855", "#E5EADB", "#324239")
+)
